eb3a34ef562b1d5731a31d2678c8ad457cd8bf19
Anthony Paul Haas <aph@lina.lanl.gov>
2022-11-22 10:25:01 -0700

    main arrays class

---
+++ src/class_main_arrays.py
@@ -0,0 +1,27 @@
+import sys
+import math
+import warnings
+import numpy as np
+from scipy.special import erf
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+
+dp = np.dtype('d')  # double precision
+dpc = np.dtype(np.cdouble) # double precision complex
+
+i4 = np.dtype('i4') # integer 4
+i8 = np.dtype('i8') # integer 8
+
+class MainArrays:
+    """
+    This class define a general baseflow object
+    """
+    def __init__(self, n_re, n_alp): # here I pass 4*ny for regular Navier-Stokes Stability and 5*ny for "Rayleigh-Taylor" Navier-Stokes
+        """
+        Constructor for class MainArrays
+        """
+        # Initialize arrays
+        self.omega_array = np.zeros((n_re, n_alp), dpc)
+
+
