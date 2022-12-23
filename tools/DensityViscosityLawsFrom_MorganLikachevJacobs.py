2ecc8720e1028489c999a06958754c685d9b4d9b
Anthony Paul Haas <aph@lina.lanl.gov>
2022-12-23 16:38:50 -0700

    minor update to DensityViscosityLawsFrom_MorganLikachevJacobs.py

--- tools/DensityViscosityLawsFrom_MorganLikachevJacobs.py
+++ tools/DensityViscosityLawsFrom_MorganLikachevJacobs.py
@@ -1,10 +1,25 @@
 
 import matplotlib
 import matplotlib.pyplot as plt
-
+from matplotlib import ticker
 
 from scipy.special import erf
 import numpy as np
+
+
+def fmt(x, pos):
+    a, b = '{:.1e}'.format(x).split('e')
+    b = int(b)
+    return r'${} \times 10^{{{}}}$'.format(a, b)
+
+def major_formatter(x, pos):
+    return f'{x:.3f}'
+
+
+# Set automatically font size to 14 and font to "serif"
+plt.rcParams['font.size'] = '15'
+plt.rc('font', family='serif')
+
 
 grav  = 9.81
 rho1  = 1.0
@@ -75,4 +90,91 @@ k_dim = k_nondim/Lscale
 print("k_nondim = ", k_nondim)
 print("k_dim = ", k_dim)
 
+
+# Now plot mole fractions
+X1 = 0.5 - 0.5*erf(z/delta)
+X2 = 0.5 + 0.5*erf(z/delta)
+
+ptn = ptn+1
+
+fig = plt.figure(ptn)
+ax = fig.add_subplot(111)
+ax.plot(X1, z, 'r', markerfacecolor='none', label="light")
+ax.plot(X2, z, 'b', markerfacecolor='none', label="heavy")
+
+#ax.set_title("scientific notation")
+#ax.set_yticks([0, 50, 100, 150])
+formatter = ticker.ScalarFormatter(useMathText=True)
+formatter.set_scientific(True)
+formatter.set_powerlimits((-1,1))
+ax.yaxis.set_major_formatter(formatter)
+
+ax.set_xlabel(r'mole fraction', fontsize=18)
+ax.set_ylabel(r'z', fontsize=18)
+plt.gcf().subplots_adjust(left=0.15)
+plt.gcf().subplots_adjust(bottom=0.16)
+
+plt.xlim([-0.01, 1.01])
+plt.ylim([-0.002, 0.002])
+
+plt.legend(loc="upper center")
+
+fig.show()
+
+
+
+
+
 input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ptn = ptn+1
+
+# f = plt.figure(ptn)
+# plt.plot(X1, z, 'r', markerfacecolor='none', label="light")
+# plt.plot(X2, z, 'b', markerfacecolor='none', label="heavy")
+# plt.xlabel('mole fraction', fontsize=18)
+# plt.ylabel('z', fontsize=18)
+# plt.legend(loc="upper center")
+
+# plt.xlim([-0.01, 1.01])
+# plt.ylim([-0.002, 0.002])
+
+# plt.gcf().subplots_adjust(left=0.16)
+# plt.gcf().subplots_adjust(bottom=0.16)
+
+# f.show()
+
+
+
